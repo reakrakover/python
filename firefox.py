cd94c297b95de9a1390ce6c3e709e8b805bed03c
reakrakover <reak@gefen.online>
2017-12-27 17:14:32 +0200

    Update firefox.py

--- firefox.py
+++ firefox.py
@@ -2,7 +2,7 @@ import time
 from selenium import webdriver
 
 print('TEST')
-driver = webdriver.Chrome('/usr/lib64/python2.7/chromedriver')  # Optional argument, if not specified will search path.
+driver = webdriver.Chrome('/usr/lib64/python2.7/chromedriver')
 driver.get('http://www.google.com/xhtml');
 time.sleep(5) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
