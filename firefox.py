782575d98a17b2122ef4b8f139442205ffee82f8
reakrakover <reak@gefen.online>
2017-12-27 23:39:01 +0200

    Update firefox.py

--- firefox.py
+++ firefox.py
@@ -1,12 +1,64 @@
-import time
-from selenium import webdriver
+# -*- coding: utf-8 -*-
 
-print('TEST')
-driver = webdriver.Chrome()
-driver.get('http://www.google.com/xhtml');
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException, NoSuchElementException
+import requests
+import random
+import time
+import os
+import sys
+
+class AustriaChromeScheidung(unittest.TestCase):
+
+    def setUp(self):
+
+        # Put your username and authey below
+        # You can find your authkey at cross browser testing.com/account
+        self.username = "reak@gefen.online"
+        self.authkey = "udf0108060cd35ad"
+
+        self.api_session = requests.Session()
+        self.api_session.auth = (self.username, self.authkey)
+
+        self.test_result = None
+
+        caps = {}
+
+        caps['name'] = 'Austria Chrome Scheidung'
+        caps['browserName'] = 'Chrome'
+        caps['version'] = '61'
+        caps['platform'] = 'Mac OSX 10.12'
+        caps['screenResolution'] = '1920x1200'
+
+        # start the remote browser on our server
+        self.driver = webdriver.Remote(
+            desired_capabilities=caps,
+            command_executor="http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub" % (self.username, self.authkey)
+        )
+
+        self.driver.implicitly_wait(10)
+
+    def test_at_scheidung_chrome(self):
+        # We wrap this all in a try/except so we can set pass/fail at the end
+        try:
+            # load the page url
+            self.driver.get("http://www.mywebtoolz.com")
+
+        except (AssertionError, WebDriverException, NoSuchElementException):
+
+            self.test_result = 'fail'
+            raise
+
+    def tearDown(self):
+        self.driver.quit()
+        # Here we make the api call to set the test's score.
+        # Pass it is passes, fail if an assertion fails, unset if the test didn't finish
+        if self.test_result is not None:
+            self.api_session.put('https://crossbrowsertesting.com/api/v3/selenium/' + self.driver.session_id,
+                                 data={'action': 'set_score', 'score': self.test_result})
+        time.sleep(20)
+
+
+if __name__ == '__main__':
+    unittest.main()
