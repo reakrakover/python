0b524223dd897c37bdde36407cea4e86f730e44a
reakrakover <reak@gefen.online>
2017-12-27 17:02:29 +0200

    Update firefox.py

--- firefox.py
+++ firefox.py
@@ -1,12 +1,11 @@
+import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox("~/geckodriver")
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/xhtml');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+driver.quit()
